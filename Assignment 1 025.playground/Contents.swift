eec221d0bf98d30dde50891b9a055dbd27712ff0
peemgay <peempeem2@gmail.com>
2020-03-06 01:26:28 +0700

    Fix Divide Test and print

--- Assignment 1 025.playground/Contents.swift
+++ Assignment 1 025.playground/Contents.swift
@@ -6,12 +6,11 @@ var numBer2 = Double (4)
 var sumMary = numBer1 + numBer2
 var difFerent = numBer1 - numBer2
 var multiPly = numBer1 * numBer2
-var diVide = numBer1 + numBer2
-
+var diVide = numBer1 / numBer2
 
 print("Input Number1: 5")
 print("Input Number2: 4")
-print("Summary = = 5 + 4 =",sumMary)
-print("Different = = 5 - 4 =",difFerent)
-print("Multiply = = 5 x 4 =",multiPly)
-print("Divide = = 5 / 4 =",diVide)
+print("Summary = = 5 + 4 = \(sumMary)")
+print("Different = = 5 - 4 = \(difFerent)")
+print("Multiply = = 5 x 4 = \(multiPly)")
+print("Divide = = 5 / 4 = \(diVide)")
