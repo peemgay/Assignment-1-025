7cd4a96311533030c8dc12b720e1dc0073eed1cb
peemgay <peempeem2@gmail.com>
2020-03-06 00:53:25 +0700

    Second Summary Test

--- Assignment 1 025.playground/Contents.swift
+++ Assignment 1 025.playground/Contents.swift
@@ -3,7 +3,9 @@ import UIKit
 var numBer1 = Double (5)
 var numBer2 = Double (4)
 
-print(numBer1 + numBer2)
+print("Input Number1: 5")
+print("Input Number2: 4")
+print("Summary = = 5 + 4 =",numBer1 + numBer2)
 print(numBer1 - numBer2)
 print(numBer1 * numBer2)
 print(numBer1 / numBer2)
