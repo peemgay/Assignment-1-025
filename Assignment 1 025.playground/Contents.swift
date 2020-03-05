742675022064885b9e90580bbdf82b5a0397c0cb
peemgay <peempeem2@gmail.com>
2020-03-06 01:13:36 +0700

    Third Multiply Test

--- Assignment 1 025.playground/Contents.swift
+++ Assignment 1 025.playground/Contents.swift
@@ -5,11 +5,12 @@ var numBer2 = Double (4)
 
 var Summary = numBer1 + numBer2
 var Different = numBer1 - numBer2
+var Multiply = numBer1 * numBer2
 
 
 print("Input Number1: 5")
 print("Input Number2: 4")
 print("Summary = = 5 + 4 =",Summary)
 print("Different = = 5 - 4 =",Different)
-print("Multiply = = 5 x 4 =",numBer1 * numBer2)
+print("Multiply = = 5 x 4 =",Multiply)
 print("Divide = = 5 / 4 =",numBer1 / numBer2)
