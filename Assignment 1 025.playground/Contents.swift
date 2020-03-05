05fa353a2d004773a55692d217fd1e6cf3bdafb4
peemgay <peempeem2@gmail.com>
2020-03-06 02:20:36 +0700

    First 5346

--- Assignment 1 025.playground/Contents.swift
+++ Assignment 1 025.playground/Contents.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-var numBer1 = Double (5)
+/*var numBer1 = Double (5)
 var numBer2 = Double (4)
 
 var sumMary = numBer1 + numBer2
@@ -13,7 +13,11 @@ print("Input Number2: 4")
 print("Summary = = 5 + 4 = \(sumMary)")
 print("Different = = 5 - 4 = \(difFerent)")
 print("Multiply = = 5 x 4 = \(multiPly)")
-print("Divide = = 5 / 4 = \(diVide)")
+print("Divide = = 5 / 4 = \(diVide)")*/
 
+var asTerisk = String ("*******************************")
 
+print(asTerisk)
+print("Input Currency: 5346")
+print(asTerisk)
 
