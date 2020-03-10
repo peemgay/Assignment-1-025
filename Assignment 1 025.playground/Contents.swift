5d79ff328e9eea9dcde8684d7ae5d9f304c1d9b0
panuwat15264za <adb15261526za@gmail.com>
2020-03-10 14:18:18 +0700

    แก้ไข

--- Assignment 1 025.playground/Contents.swift
+++ Assignment 1 025.playground/Contents.swift
@@ -1,38 +1,59 @@
 import UIKit
 
 /*var numBer1 = Double (5)
-var numBer2 = Double (4)
-
-var sumMary = numBer1 + numBer2
-var difFerent = numBer1 - numBer2
-var multiPly = numBer1 * numBer2
-var diVide = numBer1 / numBer2
-
-print("Input Number1: 5")
-print("Input Number2: 4")
-print("Summary = = 5 + 4 = \(sumMary)")
-print("Different = = 5 - 4 = \(difFerent)")
-print("Multiply = = 5 x 4 = \(multiPly)")
-print("Divide = = 5 / 4 = \(diVide)")*/
+ var numBer2 = Double (4)
+ 
+ var sumMary = numBer1 + numBer2
+ var difFerent = numBer1 - numBer2
+ var multiPly = numBer1 * numBer2
+ var diVide = numBer1 / numBer2
+ 
+ print("Input Number1: 5")
+ print("Input Number2: 4")
+ print("Summary = = 5 + 4 = \(sumMary)")
+ print("Different = = 5 - 4 = \(difFerent)")
+ print("Multiply = = 5 x 4 = \(multiPly)")
+ print("Divide = = 5 / 4 = \(diVide)")*/
 
 /*var asTerisk = "*******************************"
-
-print(asTerisk)
-print("Input Currency: 5346")
-print(asTerisk)
-
-var moNey = 5346*/
-let number = 10
-
-if ((number-1)%3)==0 {
-    print("Even")
-    if ((number+1)%2)==0 {
-        print("Odd")
-    }
-}
  
- else {
-    print("Error")
+ print(asTerisk)
+ print("Input Currency: 5346")
+ print(asTerisk)
+ 
+ var moNey = 5346*/
+var numBer = 5
+
+if (numBer%2)==0 {
+    print("Even")
+    numBer+=1
+    if numBer%3 == 0 && numBer%5 == 0 {
+        print("หาร 5 กับ 3 ลงตัว")
+    }
+    else if numBer%3 == 0 && numBer%5 != 0{
+        print("หาร 3 ลงตัว หาร 5 ไม่ลงตัว")
+    }
+    else if numBer%3 != 0 && numBer%5 == 0{
+        print("หาร 3 ไม้้ลงตัว หาร 5 ลงตัว")
+    }
+    else if numBer%3 != 0 && numBer%5 != 0{
+           print("หาร 3 ไม้้ลงตัว หาร 5 ไม่ลงตัว")
+       }
 }
-
-
+if (numBer%3)==0 {
+    print("Odd")
+    numBer-=1
+    if numBer%2 == 0 && numBer%6 == 0 {
+        print("หาร 2 กับ 6 ลงตัว")
+    }
+    else if numBer%2 == 0 && numBer%6 != 0{
+        print("หาร 2 ลงตัว หาร 6 ไม่ลงตัว")
+    }
+    else if numBer%2 != 0 && numBer%6 == 0{
+        print("หาร 2 ไม้้ลงตัว หาร 6 ลงตัว")
+    }
+    else if numBer%2 != 0 && numBer%6 != 0{
+        print("หาร 2 ไม่ลงตัว หาร 6 ไม่ลงตัว")
+    }
+    
+}
